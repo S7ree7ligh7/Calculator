20800dedd8107a0f759d40d8faf0fb43dd5d2ab6
Sebastian <Sebastian13524@protonmail.com>
2022-03-27 16:49:37 -0400

    final

---
+++ main.py
@@ -0,0 +1,15 @@
+def formula(job1, job2, money_wanted, taxes):
+    denominator = 40 * job1 + 40 * job2
+    denominator *= 1 - taxes
+    weeks = money_wanted / denominator
+    months = weeks / 4
+    return str(months)
+
+taxes = .2
+money_wanted = 10000
+print("Job 1 pay rate: ", end='')
+job_one = int(input())
+print("Job 2 pay rate: ", end='')
+job_two = int(input())
+print("Amount of months: ")
+print(formula(job_one, job_two, money_wanted, taxes))
