788bb1f5b78481f2863f36ea393994d369b84eb9
Sebastian <Sebastian13524@protonmail.com>
2022-03-27 16:59:26 -0400

    Final

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ def formula(job1, job2, money_wanted, taxes):
     denominator = 40 * job1 + 40 * job2
     denominator *= 1 - taxes
     weeks = money_wanted / denominator
-    months = weeks / 4
-    return str(months)
+    return weeks
 
 taxes = .2
 money_wanted = 10000
@@ -11,7 +10,11 @@ print("Job 1 pay rate: ", end='')
 job_one = float(input())
 print("Job 2 pay rate: ", end='')
 job_two = float(input())
-print("Amount of months: ")
-print(formula(job_one, job_two, money_wanted, taxes))
+
+weeks = formula(job_one, job_two, money_wanted, taxes)
+
+print("Amount of months: " + str(weeks / 4))
+print("Amount of weeks: " + str(weeks))
+print("Amount of days: " + str(weeks * 7))
 
 # Copy this to the other website
